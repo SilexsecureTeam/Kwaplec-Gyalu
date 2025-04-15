0fd3464b6b53eafe7b566a1d2ac9fa1433d16830
Caleb <calebshokunbi619@gmail.com>
2025-04-15 20:07:28 +1200

    landing

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./dist/**/*.html"],
+    theme: {
+      extend: {
+        fontFamily: {
+            monoton: ['"Monoton"', 'cursive'],
+          },
+      },
+    },
+    plugins: [],
+  };
+  
